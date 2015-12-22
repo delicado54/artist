367ed388950fb19522f725805488a5359155a703
Jonny Perl <jonnyperl@Jonnys-MacBook-Air.local>
2015-12-22 19:12:03 +0000

    test home

--- page-home.php
+++ page-home.php
@@ -7,20 +7,19 @@ if ( have_posts() ) : the_post();
     <div class="wrapper clearfix">
     
       <?php the_content(); ?>
+      
  <ul class="bxslider">
-<li><img src="http://fm.zarmi.com/wp-content/uploads/2015/12/01_neighbours-1.jpg" title="01_neighbours" /><p class="image-caption">When needed, we look out for each other</p></li>
-<li><img src="http://fm.zarmi.com/wp-content/uploads/2015/12/02_lifelong.jpg" title="02_lifelong" /><p class="image-caption">We’ve lived here our whole lives</p></li>
-<li><img src="http://fm.zarmi.com/wp-content/uploads/2015/12/03_workers.jpg" title="03_workers" /><p class="image-caption">We work from home</p></li>
-<li><img src="http://fm.zarmi.com/wp-content/uploads/2015/12/04_parents.jpg" title="04_parents" /><p class="image-caption">We put them to bed every night</p></li>
-<li><img src="http://fm.zarmi.com/wp-content/uploads/2015/12/05_smallspace.jpg" title="05_smallspace" /><p class="image-caption">We cope with the pressures of living in a small space</p></li>
-<li><img src="http://fm.zarmi.com/wp-content/uploads/2015/12/06_gardeners.jpg" title="06_gardeners" /><p class="image-caption">Our challenge is to keep things growing</p></li>
-<li><img src="http://fm.zarmi.com/wp-content/uploads/2015/12/07_streetlife.jpg" title="07_streetlife" /><p class="image-caption">The street life does more than keep us up</p></li>
-<li><img src="http://fm.zarmi.com/wp-content/uploads/2015/12/08_bataneyelid.jpg" title="08_bataneyelid" /><p class="image-caption">We like that no-one bats an eyelid here</p></li>
-<li><img src="http://fm.zarmi.com/wp-content/uploads/2015/12/09_activists.jpg" title="09_activists" /><p class="image-caption">We never take no for an answer; we fight our own corner</p></li>
-<li><img src="http://fm.zarmi.com/wp-content/uploads/2015/12/10_cavalry.jpg" title="10_cavalry" /><p class="image-caption">We hear the clatter of hooves as the Cavalry passes by</p></li>
-<li><img src="http://fm.zarmi.com/wp-content/uploads/2015/12/11_walkers.jpg" title="11_walkers" /><p class="image-caption">From here we can walk everywhere</p></li></ul>
-
-    news items (latest 3)
+<li><a href="#"><img src="http://fm.zarmi.com/wp-content/uploads/2015/12/05_smallspace.jpg" title="05_smallspace" /></a><p class="image-caption">Over The Threshold, 2011. Soho, London. Photo: French&Mottershead/Adrian Wood</p></li>
+<li><a href="#"><img src="http://fm.zarmi.com/wp-content/uploads/2015/12/laura.jpg"  /></a><p class="image-caption">The Shops Project: Laura (Fashion Boutique), 2009. Ljubljana, Slovenia. Photo: French&Mottershead/Urska Boljkovac</p></li>
+<li><a href="#"><img src="http://fm.zarmi.com/wp-content/uploads/2015/12/tuulastus.jpg" /></a><p class="image-caption">Understory 9: Tuulastas (night fishing), 2012. Kuopio, Finland. Photo: Pekka Makinen</p></li>
+<li><a href="#"><img src="http://fm.zarmi.com/wp-content/uploads/2015/12/psecho1.jpg"  /></a><p class="image-caption">The Echo Newspapers, 2007 &amp; 2010.</p></li>
+</ul>
+<h3>Latest Updates</h3>
+<ul class="news">
+      <li>Did you get to listen to our radio program? If not and you want to a 'refreshing perspective on the five stages of human decomposition' (thanks Stephen Hodge) here's a permalink: <a href="#">https://www.mixcloud.com/Resonance/clear-spot-18th-december-2015-french-mottershead/</a></li>
+      <li>Glad to hear news of 'The Perth &amp; Sydney Echo' now part of a national collection. http://cs.nga.gov.au/Detail.cfm?IRN=270480</li>
+      <li>In three days time we join the company of others in Turku, then next week the work goes (without us) to Helsinki! <a href="">http://newperformance.fi/?lang=en</a></li>
+      </ul>
   </div>
   
 </div>

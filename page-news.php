103381fb269a50043ef90d3b109f0e5e1f3bf0f5
Jonny Perl <jonnyperl@Jonnys-MacBook-Air.local>
2016-01-21 17:42:51 +0000

    news style

--- page-news.php
+++ page-news.php
@@ -11,19 +11,22 @@ if ( have_posts() ):
 
 <?php 
 $loop = new WP_Query(array('post_type' => 'post', 'posts_per_page' => '9', 'order' =>'DESC' )); ?>
-
+<ul id="grid">
 <?php 
  while ($loop->have_posts()) : $loop->the_post();
- ?>
-<article class="post" id="post-<?php the_id(); ?>">
+     $imageurl = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID),'artist-thumb' );
 
-    <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+?><li class="item">
+<a href="<?php echo get_permalink($post->ID); ?>"><div class="thumb-holder">	
+<img src="<?php if($imageurl[0]!=''): echo $imageurl[0]; else: bloginfo('template_url'); ?>/img/news_placeholder.png<?php endif;	 ?>" alt="<?php echo $post->post_title; ?>" /></div>
+<h3><em><?php 
+echo $post->post_title; ?></em></h3>
+<?php if(isset($description)):echo $description; endif; ?></a></li>
+						<?php endwhile; ?>					
+		
+</ul>
 
 
-    <?php the_excerpt(); ?>
-
-</article>
-<?php endwhile; ?>
   
 </div>
 	

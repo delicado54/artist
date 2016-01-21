3a4c21bff27bb845b8feed8e8dbaa66b9fcd62c5
Jonny Perl <jonnyperl@Jonnys-MacBook-Air.local>
2016-01-21 12:13:34 +0000

    mobile nav

--- single-works.php
+++ single-works.php
@@ -4,15 +4,15 @@ if ( have_posts() ): the_post();
 
 $sidebarlinks = get_post_meta($post->ID,'sidebarlinks', true);
    $terms = wp_get_post_terms($post->ID, 'work-type');
-        $current = false;
+        /*$current = false;
         foreach($terms as $term):
             if($term->slug=='current'):
                 $current = true;
                 break;
             endif;
         endforeach;
-    
-    if(!$current):
+    */
+    //if(!$current):
 ?>
 <ul id="filter" class="subnav">
         <li><a href="/work/archive/" data-group="all">All</a></li>
@@ -27,7 +27,7 @@ $sidebarlinks = get_post_meta($post->ID,'sidebarlinks', true);
         ?><li><a href="/work/archive/?c=<?php echo $cat->name; ?>#grid"><strong><?php echo $cat->name; ?></strong><?php //echo $cat->description; ?></a></li>
         <?php endforeach; ?>
         </ul>  
-<?php endif; ?> 
+<?php // endif; ?> 
 <div class="container clearfix">
        
             <h1><em><?php the_title(); ?></em></h1>

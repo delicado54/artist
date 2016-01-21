be5f3838fa6a82712f1ddad0d745f5f8d9dabcfe
Jonny Perl <jonnyperl@Jonnys-MacBook-Air.local>
2016-01-21 11:45:54 +0000

    news page

--- page-news.php
+++ page-news.php
@@ -8,8 +8,22 @@ get_header();
 if ( have_posts() ): 
 ?>
 <div class="container clearfix">
-    
-    <?php get_template_part('loop', 'posts'); ?>
+
+<?php 
+$loop = new WP_Query(array('post_type' => 'post', 'posts_per_page' => '10', 'order' =>'DESC' )); ?>
+
+<?php 
+ while ($loop->have_posts()) : $loop->the_post();
+ ?>
+<article class="post" id="post-<?php the_id(); ?>">
+
+    <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+
+
+    <?php the_excerpt(); ?>
+
+</article>
+<?php endwhile; ?>
   
 </div>
 	

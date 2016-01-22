80a356b77b098dc451f8319dc8b753fc341517ba
Jonny Perl <jonnyperl@Jonnys-MacBook-Air.local>
2016-01-22 21:00:06 +0000

    single text

--- single-texts.php
+++ single-texts.php
@@ -3,7 +3,7 @@ get_header();
 if ( have_posts() ): the_post();
 
 $sidebarlinks = get_post_meta($post->ID,'sidebarlinks', true);
-   $terms = wp_get_post_terms($post->ID, 'work-type');
+   $terms = wp_get_post_terms($post->ID, 'text-type');
         $current = false;
         foreach($terms as $term):
             if($term->slug=='current'):
@@ -15,16 +15,16 @@ $sidebarlinks = get_post_meta($post->ID,'sidebarlinks', true);
     if(!$current):
 ?>
 <ul id="filter" class="subnav">
-        <li><a href="/work/archive/" data-group="all">All</a></li>
+        <li><a href="/text/archive/" data-group="all">All</a></li>
         <?php // pull out categories
-        $cats = get_terms('work-type'); 
+        $cats = get_terms('text-type'); 
         //print_r($cats);
         foreach($cats as $cat):
             if($cat->slug=='current'):
 
                 continue;
             endif;
-        ?><li><a href="/work/archive/?c=<?php echo $cat->name; ?>#grid"><strong><?php echo $cat->name; ?></strong><?php //echo $cat->description; ?></a></li>
+        ?><li><a href="/text/archive/?c=<?php echo $cat->name; ?>#grid"><strong><?php echo $cat->name; ?></strong><?php //echo $cat->description; ?></a></li>
         <?php endforeach; ?>
         </ul>  
 <?php endif; ?> 
@@ -32,7 +32,7 @@ $sidebarlinks = get_post_meta($post->ID,'sidebarlinks', true);
        
             <h1><em><?php the_title(); ?></em></h1>
 
-        <article class="work<?php if($sidebarlinks ==''):?> nosidebar<?php endif ?>" id="post-<?php the_id(); ?>">
+        <article class="text<?php if($sidebarlinks ==''):?> nosidebar<?php endif ?>" id="post-<?php the_id(); ?>">
         	<div class="inner-wrapper">
 
             <?php the_content(); ?>

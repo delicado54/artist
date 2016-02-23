6267b479eeea882b88611a36f938bd18a76ed7eb
Jonny Perl <jonnyperl@Jonnys-MacBook-Air.local>
2016-02-23 22:11:25 +0000

    home

--- single.php
+++ single.php
@@ -10,7 +10,7 @@ if ( have_posts() ): the_post();
         
             <h1><?php the_title(); ?></h1>
 
-            <span class="meta">Posted <?php the_time('F jS Y') ?></span>
+            <span class="meta">Posted <?php the_time('j F Y') ?></span>
 
             <?php the_content(); ?>
           

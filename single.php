77a80050456099a5b81b602e52683d33bd63a074
Jonny Perl <jonnyperl@Jonnys-MacBook-Air.local>
2016-02-24 22:35:03 +0000

    new

--- single.php
+++ single.php
@@ -10,7 +10,16 @@ if ( have_posts() ): the_post();
         
             <h1><?php the_title(); ?></h1>
 
-            <span class="meta">Posted <?php the_time('j F Y') ?></span>
+            <span class="meta">Posted <?php the_time('j F Y');  $categories = get_the_category();
+$separator = ' ';
+$output = '';
+if ( ! empty( $categories ) ) {
+	echo ' in ';
+    foreach( $categories as $category ) {
+        $output .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" alt="' . esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ) . '">' . esc_html( $category->name ) . '</a>' . $separator;
+    }
+    echo trim( $output, $separator );
+}?></span>
 
             <?php the_content(); ?>
           

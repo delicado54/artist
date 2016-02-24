48057055899d2e4bb90f7e73067d8929ccead21e
Jonny Perl <jonnyperl@Jonnys-MacBook-Air.local>
2016-02-24 23:17:52 +0000

    fun

--- page-news.php
+++ page-news.php
@@ -14,19 +14,19 @@ $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 
 $loop = new WP_Query(array('post_type' => 'post', 'order' =>'DESC' , 'paged' => $paged)); ?>
 
-<ul id="grid">
+<ul id="news-items">
 <?php 
  while ($loop->have_posts()) : $loop->the_post();
      $imageurl = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID),'artist-thumb' );
 
-?><li class="item">
-<a href="<?php echo get_permalink($post->ID); ?>"><div class="thumb-holder">	
+?><li class="news-item">
+<a href="<?php echo get_permalink($post->ID); ?>"><div class="news-thumb-holder">	
 <img src="<?php if($imageurl[0]!=''): echo $imageurl[0]; else: bloginfo('template_url'); ?>/img/news_placeholder.png<?php endif;	 ?>" alt="<?php echo $post->post_title; ?>" /></div>
 <h3><em><?php 
 echo $post->post_title; ?></em></h3>
 <?php if(isset($description)):echo $description; endif; ?></a></li>
 <?php endwhile; ?>					
-<?php
+<li><?php
 	// Previous/next page navigation.
  next_posts_link('&laquo; Older Posts', $loop->max_num_pages);
  previous_posts_link('Newer Posts &raquo;'); 

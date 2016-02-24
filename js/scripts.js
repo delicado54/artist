8430d3a1803c418f1309130732cd93d58e8330c3
Jonny Perl <jonnyperl@Jonnys-MacBook-Air.local>
2016-02-24 22:04:32 +0000

    new

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,6 @@
             if(jQuery("#filter:not(.subnav)").length > 0) { 
                jQuery('#q').liveUpdate('#grid').focus();            
                jQuery('#filter a').click(function (e) {
-                    // TODO - do this only if screen is larger than 675
                     e.preventDefault();                     
 
                     // fill the #q element with the string from the data group attribute
@@ -16,13 +15,16 @@
                     // run liveupdate
                     jQuery('#q').liveUpdate('#grid').blur();
                     jQuery('#q').blur();
-
+                    //alert('balls');
                     // set active class
-                    jQuery('#filter a').removeClass('active');
+                    jQuery('#filter a').removeClass('active');                    
                     jQuery(this).addClass('active');
                                 
                });
             }
+
+            jQuery( "#filter li a.active" ).trigger( "click" );
+
         });
 
 

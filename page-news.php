c9f4bb6c993dd6917073e3c98e75da9c809d882e
Jonny Perl <jonnyperl@Jonnys-MacBook-Air.local>
2016-03-01 22:02:25 +0000

    fixes

--- page-news.php
+++ page-news.php
@@ -21,7 +21,7 @@ $loop = new WP_Query(array('post_type' => 'post', 'order' =>'DESC' , 'paged' => 
 
 ?><li class="news-item">
 <a href="<?php echo get_permalink($post->ID); ?>"><div class="news-thumb-holder">	
-<img src="<?php if($imageurl[0]!=''): echo $imageurl[0]; else: bloginfo('template_url'); ?>/img/news_placeholder.png<?php endif;	 ?>" alt="<?php echo $post->post_title; ?>" <?php if($imageurl[2]>299): echo 'class="portrait-thumb"'; endif; ?>/></a></div>
+<img src="<?php if($imageurl[0]!=''): echo $imageurl[0]; else: bloginfo('template_url'); ?>/img/news_placeholder.png<?php endif;	 ?>" alt="<?php echo $post->post_title; ?>" <?php if($imageurl[2]>260): echo 'class="portrait-thumb"'; endif; ?>/></a></div>
 <h3><a href="<?php echo get_permalink($post->ID); ?>"><?php 
 echo $post->post_title; ?></a></h3></li>
 <?php endwhile; ?>					

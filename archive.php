2f871cd2575126faa6ec51c1a6ec9a251cebeb0f
Jonny Perl <jonnyperl@Jonnys-MacBook-Air.local>
2016-03-01 22:17:57 +0000

    fixes

--- archive.php
+++ archive.php
@@ -32,17 +32,14 @@ get_header(); ?>
 
 			<?php
 			// Start the Loop.
-			while ( have_posts() ) : the_post();
-
 				/*
 				 * Include the Post-Format-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				 */
-				get_template_part( 'content', get_post_format() );
+     			get_template_part('loop', 'posts'); 
 
-			// End the loop.
-			endwhile;
+
 
 			// Previous/next page navigation.
 			the_posts_pagination( array(
@@ -57,7 +54,6 @@ get_header(); ?>
 
 		endif;
 		?>
-
 		</main><!-- .site-main -->
 	</section><!-- .content-area -->
 
